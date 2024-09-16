74de74137a05e967246074d055ab1cd083ef335a
MarzooqN <Marzooq.nadeem@gmail.com>
2024-09-16 16:02:36 -0400

    fixed

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from models import db
@@ -12,6 +12,16 @@ app.config.from_object(Config)
 db.init_app(app)
 jwt = JWTManager(app)
 CORS(app)
+
+app = Flask(__name__, static_folder='build', static_url_path='')
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return send_from_directory(app.static_folder, 'index.html')
 
 @jwt.expired_token_loader
 def expired_token_callback():
